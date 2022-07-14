81353529ea0a6cc537610e01f15fd73988dcb7bf
Peter Thomas Barrow <peter.barrow.93@gmail.com>
2022-07-14 13:48:00 +0100

    Development of new class: 'Constellation'. This allows for simplified creation of multiple 'Satellite' objects. In addition to this the 'Satellite' object has been extended to make use of the Satellite Communications toolbox in MATLAB now allowing for calculation of orbits to made internally without the need to reach for GMAT. Both the 'Satellite' and 'Located_Object' classes have also been reformatted/styled to shorted linewidths and added space around operators as well as both now making use of the 'inputParser' method to make these classes more flexible. Finally a 'utils' class has been added to collect some useful functions together whilst working on the three classes mentioned above.

---
+++ basic classes/Constellation.m
@@ -0,0 +1,201 @@
+%Author: Peter Barrow
+%Data: 11/7/22
+
+classdef Constellation
+    properties
+        scenario = nan;
+        Satellites = {Satellite.empty(0)};
+        N = nan;
+        startTime;
+        stopTime;
+        sampleTime;
+    end
+
+    methods
+        function Constellation = Constellation(varargin)
+
+            p = inputParser;
+
+            addParameter(p, 'source', nan);
+            addParameter(p, 'telescope', nan);
+            addParameter(p, 'startTime', nan);
+            addParameter(p, 'stopTime', nan);
+            addParameter(p, 'sampleTime', nan);
+            addParameter(p, 'TLE', '');
+            addParameter(p, 'name', '');
+            addParameter(p, 'KeplerElements', []);
+            addParameter(p, 'semiMajorAxis', [])
+            addParameter(p, 'eccentricity', []);
+            addParameter(p, 'inclination', []);
+            addParameter(p, 'rightAscensionOfAscendingNode', []);
+            addParameter(p, 'argumentOfPeriapsis', []);
+            addParameter(p, 'trueAnomaly', []);
+
+            parse(p, varargin{:});
+
+            %if any(isnan([p.Results.source, p.Results.telescope]))
+            %    error('Source and / or telescope not supplies');
+            %end
+
+            source = p.Results.source;
+            telescope = p.Results.telescope;
+
+            t_start = p.Results.startTime;
+            t_stop = p.Results.stopTime;
+            t_sample = p.Results.sampleTime;
+
+            Constellation.startTime = p.Results.startTime;
+            Constellation.stopTime = p.Results.stopTime;
+            Constellation.sampleTime = p.Results.sampleTime;
+
+
+            sma = p.Results.semiMajorAxis;
+            ecc = p.Results.eccentricity;
+            inc = p.Results.inclination;
+            raan = p.Results.rightAscensionOfAscendingNode;
+            aop = p.Results.argumentOfPeriapsis;
+            ta = p.Results.trueAnomaly;
+
+            if ~any([isdatetime(t_start), isdatetime(t_stop)])
+                error('Not supplied: startTime, stopTime and sampleTime');
+            end
+            
+            Constellation.scenario = satelliteScenario(t_start, ...
+                                                       t_stop, ...
+                                                       t_sample);
+            
+            if ~isempty(p.Results.TLE)
+                satellites = satellite(Constellation.scenario, p.Results.TLE);
+                Constellation.N = max(size(Constellation.scenario.Satellites));
+                Constellation = initialise_satellite_objects(Constellation, ...
+                                                            source, ...
+                                                            telescope);
+                return
+            end
+
+            % If we have got this far we must have some kepler elements as
+            % input in some form, we check anyway and exit if we don't
+            % small note: 'apply_kepler' is here mostly to make this all a bit
+            % easier to read and to avoid some extra levels of indentation
+            apply_kepler = false;
+            if ~isempty(p.Results.KeplerElements)
+                kepler_elements = p.Results.KeplerElements;
+                apply_kepler = true;
+            end
+
+            assert(6 == size(kepler_elements) * [0, 1]');
+
+            lengths = utils().lengths(sma, ecc, inc, raan, aop, ta);
+            %this logic is not quite right, this shouldnt be reached if the above test passes
+            %if ~isnan(all(lengths == lengths(1)))
+            %    apply_kepler = true;
+            %    kepler_elements = [sma', ecc', inc', raan', aop', ta'];
+            %end
+
+            if ~apply_kepler
+                error(['No data given, please supply one of the following', ...
+                       newline, char(9), 'a Two-Line element set', ... 
+                       newline, char(9), 'A matrix of kepler elements', ... 
+                       newline, char(9), 'An array for each kepler element']) 
+                return 
+            end
+
+            if (1 == sum(1 == size(kepler_elements)))
+                Constellation.N = 1;
+            elseif (2 == sum(1 == size(kepler_elements)))
+                Constellation.N = 6;
+            else
+                Constellation.N = size(kepler_elements) * (6 ~= size(kepler_elements))';
+            end
+
+            if ~isempty(p.Results.name)
+                names = p.Results.name;
+            else
+                names = createNames(Constellation.N);
+            end
+
+            if Constellation.N == 1;
+                Constellation = addSatelliteFromKepler(Constellation, ... 
+                                                       kepler_elements, ...
+                                                       names);
+            else
+
+                Constellation = addSatelliteFromKepler(Constellation, ...
+                                                       kepler_elements, ...
+                                                       names);
+            end
+
+            Constellation = initialise_satellite_objects(Constellation, ...
+                                                          source, ...
+                                                          telescope);
+        end
+
+        function Constellation = initialise_satellite_objects(Constellation, ...
+                                                              source, ...
+                                                              telescope)
+            for i= 1:Constellation.N
+                tb_sat = Constellation.scenario.Satellites(i);
+                sat = Satellite(source, telescope, ...
+                                startTime= Constellation.startTime, ...
+                                stopTime= Constellation.stopTime, ...
+                                sampleTime= Constellation.sampleTime, ...
+                                ToolBoxSatellite = tb_sat, ...
+                                name=matlab.lang.internal.uuid());
+                Constellation.Satellites{i} = sat;
+            end
+        end
+
+        function names = createNames(self, N)
+            names = cell(1, N);
+            for i = 1 : N
+                names{i} = string(matlab.lang.internal.uuid());
+            end
+        end
+
+        function Constellation = addSatelliteFromKepler(Constellation, kepler_mat, names)
+
+            if 1 == Constellation.N
+                [sma, ecc, inc, raan, aop, ta] = utils().splat(kepler_mat);
+
+                satellites = satellite(Constellation.scenario, ...
+                                       sma, ecc, inc, raan, aop, ta, ...
+                                       'Name', names);
+
+            else
+                for i = 1 : Constellation.N
+                    [sma, ecc, inc, raan, aop, ta] = utils().splat(kepler_mat(i,:));
+
+                    satellites = satellite(Constellation.scenario, ...
+                                           sma, ecc, inc, raan, aop, ta, ...
+                                           'Name', names{i});
+                end
+            end
+        end
+
+        function [sma, ecc, inc, raan, aop, ta] = elementsFromScenario(scenario)
+            orbitalElements = scenario.orbitalElements;
+            sma = meanmotion2semimajoraxis(orbitalElements.MeanMotion);
+            ecc = orbitalElements.Eccentricity;
+            inc = orbitalElements.Inclination;
+            raan = orbitalElements.RightAscensionOfAscendingNode;
+            aop = orbitalElements.ArgumentOfPeriapsis;
+            ta = eccentricity2trueAnomaly(ecc);
+        end
+
+        function true_anomaly = eccentricity2trueAnomaly(eccentricity)
+            true_anomaly = 2 .* atan(...
+                                    sqrt(...
+                                            (1 + eccentricity) ...
+                                            / (1 - eccentricity)) ...
+                                    .* tan(eccentricity / 2));
+        end
+
+        function semimajor_axis = meanmotion2semimajoraxis(mean_motion)
+            M = 5.97237e24;
+            G = 6.67430e-11;
+            mu = G * M;
+            
+            semimajor_axis = mu ./ (mean_motion .^ 2);
+        end
+    end
+end
--- basic classes/Located_Object.m
+++ basic classes/Located_Object.m
@@ -1,212 +1,237 @@
+%Author: Cameron Simmons, Peter Barrow
+%Date: 24/1/22
+
+
+% To-DO
+% - Add velocity calculations
+%   - Satellite toolbox already returns velocities to us, just needs a coord transform
+%   - Would be convenient to have a function that can calculated the relative
+%       velocity between two objects, especially useful for apparent satellite
+%       motion relative to a ground station
+
 classdef (Abstract=true)Located_Object
-    %LOCATED_OBJECT object containing latitude, longitude and altitude position data,
+    %object containing latitude, longitude and altitude position data,
     %possibly for many time stamps
 
     properties(SetAccess=protected)
-        Latitude(:,1){mustBeNumeric}=nan;                                  %N dimensional column vector containing latitude
-        Longitude(:,1){mustBeNumeric}=nan;                                 %N dimensional column vector containing longitude
-        Altitude(:,1){mustBeNumeric}=nan;                                  %N dimensional column vector containing altitude
-        Location_Name{mustBeText}='';                                      %name of location
+        Latitude(:,1){mustBeNumeric} = nan;
+        Longitude(:,1){mustBeNumeric} = nan;
+        Altitude(:,1){mustBeNumeric} = nan;
+        Location_Name = "";
     end
-    properties(SetAccess=protected,Hidden=true)
-        N_Position{mustBeInteger,mustBePositive}=1;                        %number of entries in LLA_Array
+    properties(SetAccess=protected, Hidden=true)
+        % Length of Latitude, Longitude, Altitude arrays
+        % (must all be same length)
+        N_Position{mustBeInteger, mustBePositive} = 1;
     end
-    properties(Constant=true,Hidden=true)
-        Earth_Radius=earthRadius()                                         %radius of the earth in m (used to compute shadowing)
+    properties(Constant=true, Hidden=true)
+        % radius of the earth in m (used to compute shadowing)
+        Earth_Radius = earthRadius()
     end
 
     methods
         function LLA = GetLLA(Located_Object)
             %GETLLA return latitude, longitude and altitude as an nx3 array
-            LLA = [Located_Object.Latitude,Located_Object.Longitude,Located_Object.Altitude];
+            LLA = [Located_Object.Latitude, ...
+                   Located_Object.Longitude, ...
+                   Located_Object.Altitude];
         end
 
-        function Located_Object=SetPosition(Located_Object,varargin)
+        function Located_Object = SetPosition(Located_Object, varargin)
             %%SETPOSITION set the position properties of a location object
 
-            switch nargin
-                case 2
-                    %if 1 input, assume input is an nx3 array
-                    LLA=varargin{1};
-                    sz=size(LLA);%check dimensions of LLA
-                    if ~sz(2)==3
-                        error('LLA array must be nx3')
-                    else
-                        Located_Object.N_Position=sz(1);
-                    end
+            %disp(nargin);
 
-                    Located_Object.Latitude=LLA(1);
-                    Located_Object.Longitude=LLA(2);
-                    Located_Object.Altitude=LLA(3);
-                case 3
-                    %if 2 inputs, assume input1 is an nx3 array and 2 is a
-                    %name
-                    LLA=varargin{1};
-                    sz=size(LLA);%check dimensions of LLA
-                    if ~sz(2)==3
-                        error('LLA array must be nx3')
-                    else
-                        Located_Object.N_Position=sz(1);
-                    end
+            %if ~(nargin > 1 && nargin < 6)
+            %    error('Recieved:\t%d arguments\n\tExpected [1 -> 6] as input (1 positional, 4 optional)');
+            %end
 
-                    Located_Object.Latitude=LLA(1);
-                    Located_Object.Longitude=LLA(2);
-                    Located_Object.Altitude=LLA(3);
-                    Located_Object.Location_Name=varargin{2};
-                case 4
-                    %if 3 inputs, assume latitude, longitude and altitude
-                    if ~AreSameSize(varargin{1},varargin{2},varargin{3})
-                        error('latitude, longitude and altitude must be same sized vectors');
-                    else
-                        Located_Object.N_Position=numel(varargin{1});
-                    end
-                    Located_Object.Latitude=varargin{1};
-                    Located_Object.Longitude=varargin{2};
-                    Located_Object.Altitude=varargin{3};
-                case 5
-                    %if 4 inputs, assume lat,lon,alt and name
-                    if ~AreSameSize(varargin{1},varargin{2},varargin{3})
-                        error('latitude, longitude and altitude must be same sized vectors');
-                    else
-                        Located_Object.N_Position=numel(varargin{1});
-                    end
-                    Located_Object.Latitude=varargin{1};
-                    Located_Object.Longitude=varargin{2};
-                    Located_Object.Altitude=varargin{3};
-                    Located_Object.Location_Name=varargin{4};
-                otherwise
-                    error('must provide latitude, longitude and altitude with optional location name separately')
+            p = inputParser;
+
+            addRequired(p, 'Located_Object');
+            addParameter(p, 'LLA', nan, @isnumeric);
+            addParameter(p, 'Latitude', nan, @isnumeric);
+            addParameter(p, 'Longitude', nan, @isnumeric);
+            addParameter(p, 'Altitude', nan, @isnumeric);
+            addParameter(p, 'Name', "");
+
+            parse(p, Located_Object, varargin{:});
+
+            LLA = p.Results.LLA;
+            lat = p.Results.Latitude;
+            lon = p.Results.Longitude;
+            alt = p.Results.Altitude;
+
+            Located_Object.Location_Name = p.Results.Name;
+
+            initialised = false;
+
+            if ~isnan(LLA)
+                Located_Object.Latitude = LLA(1);
+                Located_Object.Longitude = LLA(2);
+                Located_Object.Altitude = LLA(3);
+                initialised = true;
+            end
+            
+            if AreSameSize(lat, lon, alt)
+                Located_Object.Latitude = lat;
+                Located_Object.Longitude = lon;
+                Located_Object.Altitude = alt;
+                Located_Object.N_Position = numel(lat);
+                initialised = true;
+            else
+                error('latitude, longitude and altitude must be same sized vectors');
+            end
+
+            if initialised == true
+                return
+            else
+                error('Failed to initialise object: LLA or latitude, longitude altitude vectors incorrect format');
             end
         end
 
-        function ENUs=ComputeRelativeCoords(Located_Obj_1,Located_Obj_2)
+        function ENUs = ComputeRelativeCoords(Located_Obj_1, Located_Obj_2)
             %%COMPUTERELATIVECOORDS compute the ENU coords of located
             %%object 1 relative to located object 2
 
-            %% need to vary behaviour dependent on number of position data time stamps in each object
-            if Located_Obj_1.N_Position==1&&Located_Obj_2.N_Position==1
-                %1 location from each object
-                ENUs=lla2enu(GetLLA(Located_Obj_1),GetLLA(Located_Obj_2),'ellipsoid');
+            %1 location from each object
+            if Located_Obj_1.N_Position == 1 && Located_Obj_2.N_Position == 1
+                ENUs = lla2enu(GetLLA(Located_Obj_1), ...
+                                      GetLLA(Located_Obj_2), ...
+                                      'ellipsoid');
+                return
+            end
 
-            elseif Located_Obj_1.N_Position>1&&Located_Obj_2.N_Position==1
-                % multiple locations from first object
-                ENUs=zeros(Located_Obj_1.N_Position,3);
-                LLA1=GetLLA(Located_Obj_1);
-                for i=1:Located_Obj_1.N_Position
-                    ENUs(i,:)=lla2enu(LLA1(i,:),GetLLA(Located_Obj_2),'ellipsoid');
+            % multiple locations from first object
+            if Located_Obj_1.N_Position > 1 && Located_Obj_2.N_Position == 1
+                ENUs = zeros(Located_Obj_1.N_Position, 3);
+                LLA1 = GetLLA(Located_Obj_1);
+
+                for i = 1:Located_Obj_1.N_Position
+                    ENUs(i,:)=lla2enu(LLA1(i,:), ...
+                                      GetLLA(Located_Obj_2), ...
+                                      'ellipsoid');
                 end
+                return;
+            end
 
-            elseif Located_Obj_1.N_Position==1&&Located_Obj_2.N_Position>1
-                % multiple locations from second object
-                ENUs=zeros(Located_Obj_2.N_Position,3);
-                LLA2=GetLLA(Located_Obj_2);
-                for i=1:Located_Obj_2.N_Position
-                    ENUs(i,:)=lla2enu(GetLLA(Located_Obj_1),LLA2(i,:),'ellipsoid');
+            % multiple locations from second object
+            if Located_Obj_1.N_Position == 1 && Located_Obj_2.N_Position > 1
+                ENUs = zeros(Located_Obj_2.N_Position, 3);
+                LLA2 = GetLLA(Located_Obj_2);
+
+                for i = 1:Located_Obj_2.N_Position
+                    ENUs(i,:) = lla2enu(GetLLA(Located_Obj_1), ...
+                                      LLA2(i,:), ...
+                                      'ellipsoid');
                 end
+                return;
+            end
 
-            elseif Located_Obj_1.N_Position>1&&Located_Obj_2.N_Position>1
-                % multiple locations from both objects
-                if Location_Obj_1.N_Position~=Location_Obj_2.N_Position
+            % multiple locations from both objects
+            if Located_Obj_1.N_Position > 1 && Located_Obj_2.N_Position > 1
+                if Location_Obj_1.N_Position ~= Location_Obj_2.N_Position
                     error('location objects provided do not have same number of position entries')
                 end
 
-                ENUs=zeros(Located_Obj_2.N_Position,3);
-                LLA1=GetLLA(Located_Obj_1);
-                LLA2=GetLLA(Located_Obj_2);
-                for i=1:Located_Obj_2.N_Position
-                    ENUs(i,:)=lla2enu(LLA1(i,:),LLA2(i,:),'ellipsoid');
+                ENUs = zeros(Located_Obj_2.N_Position, 3);
+                LLA1 = GetLLA(Located_Obj_1);
+                LLA2 = GetLLA(Located_Obj_2);
+
+                for i = 1:Located_Obj_2.N_Position
+                    ENUs(i,:) = lla2enu(LLA1(i,:), ...
+                                        LLA2(i,:), ...
+                                        'ellipsoid');
                 end
-            else
-                error('can provide objects with the same number of position entries, and/or one with a single position entry')
+                return;
             end
 
+            error('can provide objects with the same number of position entries, and/or one with a single position entry')
+            ENUs = nan;
         end
 
-        function Distance=ComputeDistanceBetween(Located_Obj_1,Located_Obj_2)
+        function Distance = ComputeDistanceBetween(Located_Obj_1, Located_Obj_2)
             %%COMPUTEDISTANCEBETWEEN return the distance(s) in m between
             %%the two located objects
 
             %compute relative coords
-            ENUs=ComputeRelativeCoords(Located_Obj_1,Located_Obj_2);
+            ENUs = ComputeRelativeCoords(Located_Obj_1, Located_Obj_2);
             %take magnitude for distances
-            Distance=Row2Norms(ENUs);
+            Distance = Row2Norms(ENUs);
         end
 
-        function [Headings,Elevations,Distances]=RelativeHeadingAndElevation(Located_Obj_1,Located_Obj_2)
-            %%RELATIVEHEADINGANDELEVATION return the heading and elevation
+        function [H, E, D] = RelativeHeadingAndElevation(Located_Obj_1, ...
+                                                         Located_Obj_2)
+            %%RELATIVEHEADINGANDELEVATION returh the heading and elevation
+            % [H, E, D] == [Headings, Elevations, Distances]
             %%of object 1 relative to object 2
             %% get ENU of 1 from 2
-            ENUs=ComputeRelativeCoords(Located_Obj_1,Located_Obj_2);
+            ENUs = ComputeRelativeCoords(Located_Obj_1, Located_Obj_2);
 
             %% compute heading and elevation from ground station
-            [Headings,Elevations]=HeadingAndElevation(ENUs);
+            [H, E] = HeadingAndElevation(ENUs);
 
             %% transpose into row vectors
-            Headings=Headings';
-            Elevations=Elevations';
+            H = H';
+            E = E';
 
             %% if needed, compute distances
-            Distances=Row2Norms(ENUs);
+            Distances = Row2Norms(ENUs);
         end
 
-        function XYZcoords=GetXYZ(Located_obj_1)
+        function [X, Y, Z] = GetXYZ(Loc_obj)
             %%GETXYZ return the position of the located object wrt to the
             %%central coordinate system of the earth
             %%(https://www.nosco.ch/mathematics/en/earth-coordinates.php#:~:text=The%20usual%20cartesian%20coordinate%20system%20associated%20with%20the,its%20positive%20direction%20is%20towards%20the%20north%20pole.)
 
-            %find lat, lon, alt
-            Latitude=Located_obj_1.Latitude; %#ok<*PROP>
-            Longitude=Located_obj_1.Longitude;
-            Altitude=Located_obj_1.Altitude;
-            Local_Radius=(earthRadius+Altitude);
+            Local_Radius = earthRadius + Loc_obj.Altitude;
             % convert to XYZ
-            X=Local_Radius.*cosd(Latitude).*cosd(Longitude);
-            Y=Local_Radius.*cosd(Latitude).*sind(Longitude);
-            Z=Local_Radius.*sind(Latitude);
-
-            %output
-            XYZcoords=[X,Y,Z];
+            X = Local_Radius .* cosd(Loc_obj.Latitude) .* cosd(Loc_obj.Longitude);
+            Y = Local_Radius .* cosd(Loc_obj.Latitude) .* sind(Loc_obj.Longitude);
+            Z = Local_Radius .* sind(Loc_obj.Latitude);
         end
 
-        function ShadowFlag=IsEarthShadowed(Located_obj_1,Located_obj_2)
+        function ShadowFlag = IsEarthShadowed(Located_obj_1, Located_obj_2)
             %%ISEARTHSHADOWED return a flag which is true if the line of
             %%sight between these objects runs below the earth's surface
             %%and therefore the earth shadows their intervisibility
 
             %% get the two XYZ positions of the two objects
-            Pos_1=GetXYZ(Located_obj_1);
-            Pos_2=GetXYZ(Located_obj_2);
+            Pos_1 = GetXYZ(Located_obj_1);
+            Pos_2 = GetXYZ(Located_obj_2);
 
             %% determine the minimum radius from earth's centre of the line between these two
-            Dot_product=sum(Pos_1.*Pos_2,2);
-            Lambda_min=(Row2Norms(Pos_1).^2-Dot_product)./(Row2Norms(Pos_1).^2+Row2Norms(Pos_2).^2-2.*Dot_product); %nondimensional parameter descrbing position of minimum radius point
-            Pos_min=Pos_1.*(1-Lambda_min)+Pos_2.*Lambda_min;
+            Dot_product = sum(Pos_1 .* Pos_2,2);
+            Lambda_min = (Row2Norms(Pos_1).^2 ...
+                          - Dot_product) ./ ...
+                         (Row2Norms(Pos_1).^2 ...
+                          + Row2Norms(Pos_2).^2 ...
+                          - 2 .* Dot_product);
+
+            Pos_min = Pos_1 .* (1 - Lambda_min) + Pos_2 .* Lambda_min;
             %check lambda for not being inside bounds
-                %if Lambda_min is <0 this indicates the the first position
-                %is the lowest radius
-                %Pos_1 may be an array or a vector
-                if isvector(Pos_1)
-                Pos_min(Lambda_min<0,:)=ones(sum(Lambda_min<0),1)*Pos_1;
-                else
-                Pos_min(Lambda_min<0,:)=Pos_1(Lambda_min<0,:);
-                end
-                %if lambda_min>1 this indicates that the 2nd position is
-                %the lowest radius
-                if isvector(Pos_2)
-                Pos_min(Lambda_min>1,:)=ones(sum(Lambda_min>1),1)*Pos_2;
-                else
-                Pos_min(Lambda_min>1,:)=Pos_2(Lambda_min>1,:);
-                end
+            %if Lambda_min is <0 this indicates the the first position
+            %is the lowest radius
+            %Pos_1 may be an array or a vector
+            if isvector(Pos_1)
+                Pos_min(Lambda_min<0,:) = ones(sum(Lambda_min<0),1) * Pos_1;
+            else
+                Pos_min(Lambda_min<0,:) = Pos_1(Lambda_min<0,:);
+            end
+            %if lambda_min>1 this indicates that the 2nd position 9is
+            %the lowest radius
+            if isvector(Pos_2)
+                Pos_min(Lambda_min>1,:) = ones(sum(Lambda_min>1),1) * Pos_2;
+            else
+                Pos_min(Lambda_min>1,:) = Pos_2(Lambda_min>1,:);
+            end
 
             %produce a located_object at this minimum point by converting
             %to spherical coords and then geographic coords
-            R_min=Row2Norms(Pos_min);
+            R_min = Row2Norms(Pos_min);
 
             %% if R_min is less than earth radius, shadowing is present
-            ShadowFlag=R_min<Located_obj_1.Earth_Radius;
+            ShadowFlag = R_min < Located_obj_1.Earth_Radius;
         end
-
     end
-
-
 end
