d801867b627dbbfbfe2995d798abd9c2f527c0b2
Peter Thomas Barrow <peter.barrow.93@gmail.com>
2022-11-07 16:45:13 +0000

    Finalising calculation of sky photons.

---
+++ basic classes/sky_photons.m
@@ -0,0 +1,13 @@
+% Author: Peter Barrow
+%
+% Ref: Gruneisen, M. T., Eickhoff, M. L., et al. (2021),
+% Adaptive-Optics-Enabled Quantum Communication: A Technique for Daytime
+% Space-To-Earth Links, 10.1103/PhysRevApplied.16.014067.
+%
+% Calculate the number of sky photons coupled into the telescope
+
+function N = sky_photons(radiance, FOV, rx_diameter, wavelength, filter_width,
+    integration_time)
+    N = ( radiance .* FOV .* pi .* (rx_diameter .^ 2) .* wavelength ...
+          .* filter_width .* integration_time ) ./ (4 * h * c);
+end
