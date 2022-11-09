e5136cdc35521674cb6aadf67ea637fcee8b9a6d
Peter Thomas Barrow <peter.barrow.93@gmail.com>
2022-11-09 15:41:33 +0000

    Clean up / additions of missing features. Added detector saturation and rate dependent jitter to all protocols. Model for DPS is currently missing from repo.

--- BB84 implementations/private/BB84_single_photon_model.m
+++ BB84 implementations/private/BB84_single_photon_model.m
@@ -1,6 +1,3 @@
-function [secret_key_rate, qber] = BB84_single_photon_model(MPN, g2, state_prep_error, rep_rate,...
-    det_eff, prob_dark_counts, loss, prot_eff, qber_jitter, dead_time, polarisation_error)
-
 % Function to compute sifted key rate and QBER of the BB84 protocol with
 % single photon sources
 % -------------------------------------------------------------------
@@ -31,77 +28,88 @@ function [secret_key_rate, qber] = BB84_single_photon_model(MPN, g2, state_prep_
 % qber = QBER of the transmission system [%]
 % ########################################
 
+function [secret_key_rate, qber, Rate_In, Rate_Det] = ...
+                                                BB84_single_photon_model(...
+                                                    MPN, ...
+                                                    g2, ...
+                                                    state_prep_error, ...
+                                                    rep_rate,...
+                                                    det_eff, ...
+                                                    prob_dark_counts, ...
+                                                    loss, ...
+                                                    prot_eff, ...
+                                                    qber_jitter, ...
+                                                    dead_time, ...
+                                                    polarisation_error, ...
+                                                    Detector)
 
-% detection efficiency
-eta = det_eff;
-if det_eff > 1
-    eta = det_eff / 100;
+    % detection efficiency
+    eta = det_eff;
+    if det_eff > 1
+        eta = det_eff / 100;
+    end
+    
+    % transmission channel's loss
+    loss = 10.^(-loss/10);
+    
+    % probability of dark counts (Bob's detetcion stage - convolution of all 
+    % detectors used by Bob)
+    prob_dark = prob_dark_counts;
+    
+    % probability of a single detection event 
+    prob_click = MPN * eta * loss+ prob_dark;
+    
+    % probability that the source generated more than one photon
+    prob_multi = 0.5 * MPN.^2 * eta.^2 * g2;
+    
+    % fraction of detection events originating from single photons
+    beta = (prob_click - prob_multi) ./ prob_click;
+    
+    %convolution of state preparation errors
+    %( imperfect polarisation optics, channel dechoerence, imperfect state 
+    % preparation )
+    mu = state_prep_error;
+    
+    % probability of a signal event (Bob's estimation based on Alice's 
+    % original signal)
+    prob_signal = MPN * loss;
+    
+    % QBER
+    % qber due to polarisation compensation error is the sine of the mean error
+    % angle (in degrees)
+
+    Rate_In = rep_rate .* prob_click;
+    Detector = SetJitterPerformance(Detector, Rate_In);
+    qber_jitter = Detector.QBER_Jitter;
+    qber_polarisation_error = sind(polarisation_error);
+    
+    % add up qbers. since small, neglect overlapping probabilities
+    qber = (mu * prob_signal + prob_dark * 0.5) ./ prob_click + qber_jitter ...
+            + qber_polarisation_error;
+    % CS modified qber cannot exceed 0.5
+    qber(qber > 0.5) = 0.5;
+    
+    % compression function (account's for Eve's attacks on the raw quantum key)
+    tau = -log2(0.5 + 2*qber./beta - 2*(qber./beta).^2);
+    % cs modified if qber./beta>1+ a bit, then tau is complex. In this case 
+    % tau should return 0
+    tau(~(imag(tau)==0))=0;
+    
+    % function characterising the performance of the error correction algorithm
+    % [f(QBER) is always >= 1 where the equality holds for a lossless/perfect 
+    % algorithm], this has been extrapolated from the paper
+    f = 1.366 .* qber + 1.117;
+    
+    % binary entropy function, a.k.a. h(x)
+    bin_ent = -qber.*log2(qber) - (1-qber).*log2(1-qber);
+    
+    % secret key rate
+    rate = rep_rate .* prot_eff .* prob_click;
+    Rate_Det = dead_time_corrected_count_rate(rate, dead_time, 1);
+    secret_key_rate = rate .* (beta .* tau - f .* bin_ent);
+    secret_key_rate(secret_key_rate < 0) = 0; 
+    % cs modification: output sifted key rate of zero in place of nan when 
+    % calculation returns zero
+    % altered 1/dead time limit to allow vectorised operation
+
 end
-
-% transmission channel's loss
-loss = 10.^(-loss/10);
-
-% probability of dark counts (Bob's detetcion stage - convolution of all detectors used by Bob)
-prob_dark = prob_dark_counts;
-
-% probability of a single detection event 
-prob_click = MPN * eta * loss+ prob_dark;
-
-% probability that the source generated more than one photon
-prob_multi = 0.5 * MPN.^2 * eta.^2 * g2;
-
-% fraction of detection events originating from single photons
-beta = (prob_click - prob_multi) ./ prob_click;
-
-%convolution of state preparation errors
-%(imperfect polarisation optics, channel dechoerence, imperfect state preparation)
-mu = state_prep_error;
-
-% probability of a signal event (Bob's estimation based on Alice's original signal)
-prob_signal = MPN * loss;
-
-% QBER
-%qber due to polarisation compensation error is the sine of the mean error
-%angle (in degrees)
-qber_polarisation_error = sind(polarisation_error);
-
-%add up qbers. since small, neglect overlapping probabilities
-qber = (mu * prob_signal + prob_dark * 0.5) ./ prob_click + qber_jitter + qber_polarisation_error;
-%% cs modified qber cannot exceed 0.5
-qber(qber>0.5)=0.5;
-
-% compression function (account's for Eve's attacks on the raw quantum key)
-tau = -log2(0.5 + 2*qber./beta - 2*(qber./beta).^2);
-%% cs modified if qber./beta>1+ a bit, then tau is complex. In this case tau should return 0
-tau(~(imag(tau)==0))=0;
-
-% function characterising the performance of the error correction algorithm
-% [f(QBER) is always >= 1 where the equality holds for a lossless/perfect algorithm]
-% [this has been extrapolated from the paper]
-f = 1.366 .* qber + 1.117;
-
-% binary entropy function, a.k.a. h(x)
-bin_ent = -qber.*log2(qber) - (1-qber).*log2(1-qber);
-
-% secret key rate
-rate = rep_rate .* prot_eff .* prob_click;
-%sifted_key_rate = rep_rate .* prot_eff .* prob_click;
-%sifted_key_rate(sifted_key_rate>1/dead_time)=1/dead_time;
-sifted_key_rate = dead_time_corrected_count_rate(rate, dead_time, 1);
-secret_key_rate = sifted_key_rate.* (beta .* tau - f .* bin_ent);
-secret_key_rate(secret_key_rate<0) = 0; 
-%% cs modification: output sifted key rate of zero in place of nan when calculation returns zero
-%% altered 1/dead time limit to allow vectorised operation
-
-end
-
-
-
-
-
-
-
-
-
-
-
--- COW implementations/private/COW_model.m
+++ COW implementations/private/COW_model.m
@@ -3,45 +3,62 @@
 % P. Sibson, C. Erven, M. Godfrey et al.
 
 %altered by Cameron Simmons
-function [SKR_COW_2008, QBER] = COW_model(MPN, State_Prep_Error, rep_rate, prob_dark_counts, loss, QBER_Jitter, dead_time, decoy_prob, V)
+% further altered by Peter Barrow
+function [SKR_COW_2008, QBER, R_In, R_Det] = COW_model(MPN, ...
+                                                       State_Prep_Error, ...
+                                                       rep_rate, ...
+                                                       prob_dark_counts, ...
+                                                       loss, ...
+                                                       QBER_Jitter, ...
+                                                       dead_time, ...
+                                                       decoy_prob, ...
+                                                       V, ...
+                                                       Detector)
 
-
-
-    f = 1.2;                                                               %error correction efficiency
-    T   = 10.^(-loss/10);                                                  %Total absolute loss
-    R   = MPN.*T;                                                          %average number of photons arriving at receiver per pulsee
-
-    P_click = R + prob_dark_counts;                           % probability of pings at receiver
+    %error correction efficiency
+    f = 1.2;
+    % Total absolute loss
+    T = 10.^(-loss/10);
+    % average number of photons arriving at receiver per pulsee
+    R = MPN.*T;
+    % probability of pings at receiver
+    P_click = R + prob_dark_counts;
     
-    R_sifted = 0.5*R*rep_rate + prob_dark_counts*rep_rate;   %frequency of pings at the receiver
+    % frequency of pings at the receiver
+    Rates_In = 0.5 * R * rep_rate + prob_dark_counts * rep_rate;
     %R_sifted = min(R_sifted, 1/dead_time);
-    R_sifted = dead_time_corrected_count_rate(R_sifted, dead_time, 1);
+    R_sifted = dead_time_corrected_count_rate(Rates_In, dead_time, 1);
+    Rates_Det = R_sifted
 
     %% QBER totalling
-    QBER_dark       =  0.5*prob_dark_counts./P_click;
+    Detector = SetJitterPerformance(Detector, Rates_In);
+    QBER_Jitter = Detector.QBER_Jitter;
+    QBER_dark = 0.5 * prob_dark_counts ./ P_click;
     
     %each QBER is a probability. These probabilities are independent
-    %(coming from different sources). Therefore QBERs add in union
-
-    %in other words, the probability of an errorless bit (Quantum No Bit Error Rate QNBER) is the product of the probabilities of no errors from each source 
-    QNBER=(1-QBER_dark)*(1-QBER_Jitter)*(1-State_Prep_Error);
-    QBER=1-QNBER;
+    %(coming from different sources). Therefore QBERs add in union, in other 
+    % words, the probability of an errorless bit (Quantum No Bit Error Rate 
+    % QNBER) is the product of the probabilities of no errors from each source 
+    QNBER = (1 - QBER_dark) * (1 - QBER_Jitter) * (1 - State_Prep_Error);
+    QBER = 1 - QNBER;
     
     %% Privacy amplification stage
     Xcow = QBER + (1 - QBER).*H((1 + eps(MPN, V))/2);                      
     
-    %% Secure key rate
-    SKR_COW_2008 = R_sifted.*(1 - f*H(QBER) - Xcow).*(1-decoy_prob);       %secure key rate
-    SKR_COW_2008(SKR_COW_2008<0)=0;                                        %SKR cannot be negative
+    % Secure key rate
+    SKR_COW_2008 = R_sifted.*(1 - f*H(QBER) - Xcow).*(1-decoy_prob);
+    % SKR cannot be negative
+    SKR_COW_2008(SKR_COW_2008<0)=0;
 end
 
-%% Utils functions
+% Utils functions
 function [V_] = eps(mu,V)
     V_ = (2*V - 1)*exp(-mu) - 2*sqrt(V*(1 - V))*sqrt(1 - exp(-2*mu));
 end
 
 function [entropy] = H(p)
-    assert(all(p>=0& p<=1),'input to binary entropy function must be a valid probability')
+    assert( all(p >= 0 & p <= 1), ...
+        'input to binary entropy function must be a valid probability' );
 
-    entropy=-(p.*log2(p)+(1-p).*log2(1-p));
+    entropy = -( p .* log2(p) + (1 - p) .* log2(1 - p) );
 end
