dee123fa34d534ba56ce74568bab9cabeac5d07b
Peter Thomas Barrow <peter.barrow.93@gmail.com>
2022-11-23 16:59:20 +0000

    Ability to generate atmosphere for a location given a base SMARTS configuration. Stores the global tilted irrandiance and transmission to a .mat file at specified location.

--- Example scripts/create_atmosphere.m
+++ Example scripts/create_atmosphere.m
@@ -8,3 +8,37 @@ results_path = '/home/bp38/Documents/MATLAB/SMARTS/gen_atmosphere/';
 config = solar_background_errol(executable_path=smarts_path, stub=results_path);
 
 genAtmos = GenerateAtmosphere(config)
+
+%% read files
+
+
+atmosphere = cell(numel(genAtmos.azimuth), numel(genAtmos.elevation));
+
+for i = 1 : numel(genAtmos.azimuth)
+    a = genAtmos.azimuth(i);
+    for j = 1 : numel(genAtmos.elevation)
+        e = genAtmos.azimuth(j);
+        fp = convertCharsToStrings(genAtmos.result_files{i, j}) + ".ext.txt";
+        disp(fp)
+        data = readtable(fp, 'VariableNamingRule', 'preserve');
+        temp.azimuth = a;
+        temp.elevation = e;
+        temp.transmittance = data.Mixed_gas__transmittance;
+        temp.global_tilted_irradiance = data.Global_tilted_irradiance;
+        atmosphere{i, j} = temp;
+    end
+end
+
+%%
+
+a = genAtmos.azimuth;
+e = genAtmos.elevation;
+
+A = linspace(1, 4, 4);
+E = linspace(1, 3, 3);
+
+[AE, EE] = meshgrid(A, E);
+opts = [AE(:), EE(:)]
+opts(2, :)
+reshape(opts, [3, 4, 2])
+EE
--- SMARTS_connection/GenerateAtmosphere.m
+++ SMARTS_connection/GenerateAtmosphere.m
@@ -8,40 +8,75 @@ classdef GenerateAtmosphere
 
     properties
         
+        azimuth;
+        elevation;
         SMARTS_conf SMARTS_input;
         result_files;
+        atmosphere;
 
     end
 
 
     methods
 
-        function GenerateAtmosphere = GenerateAtmosphere(SMARTS_conf)
+        function GenerateAtmosphere = GenerateAtmosphere(SMARTS_conf, varargin)
+
+            p = inputParser;
+            addParameter(p, 'step_size_aziumth', 5);
+            addParameter(p, 'step_size_elevation', 5);
+            parse(p, varargin{:});
 
             assert(isa(SMARTS_conf, 'SMARTS_input'), 'Must be a SMARTS_input');
 
-            spectral_reflectance = SMARTS_conf.ialbdx.spectral_reflectance
+            GenerateAtmosphere.SMARTS_conf = SMARTS_conf;
+
+            spectral_reflectance = SMARTS_conf.ialbdx.spectral_reflectance;
             foreground_local_albedo = SMARTS_conf.ialbdx.local_foreground_albedo;
 
-            azimuth = linspace(0, 360, 10);
-            elevation = linspace(0, 90, 10);
+            arange = @(START, STOP, STEP) linspace( START, ...
+                                                    STOP - STEP, ...
+                                                    STOP / STEP );
+
+            GenerateAtmosphere.azimuth = arange(0, 360, p.step_size_aziumth);
+            GenerateAtmosphere.elevation = arange(0, 90, p.step_size_elevation);
+
+            GenerateAtmosphere = GenerateAtmosphere.SimulateAtmosphere();
+            GenerateAtmosphere = GenerateAtmosphere.ConstructAtmosphere();
+
+        end
+
+        function GenerateAtmosphere = SimulateAtmosphere(GenerateAtmosphere)
+
+            azimuth = GenerateAtmosphere.azimuth;
+            elevation = GenerateAtmosphere.elevation;
+
             GenerateAtmosphere.result_files = cell(numel(azimuth), numel(elevation));
 
-            imass = solar_position_and_airmass(dateAndTime=datetime(2022, 8, 15, 12, 0, 0), ...
-                                    latitude=56.405, longitude=-3.183);
-
-            SMARTS_conf = SMARTS_conf.update_card(imass);
+            extensions = [".inp.txt", ".ext.txt", ".out.txt"];
+            file_exist = @(F) arrayfun(@(F) exist(F, 'file'), F + extensions);
 
             for a = 1 : numel(azimuth)
                 for e = 1 : numel(elevation)
-                    ialbdx = far_field_albedo(...
+
+                    file_name = [ 'az_', num2str(azimuth(a)), ...
+                                  '_el_', num2str(elevation(e)) ];
+
+                    file_path = [SMARTS_conf.file_path_stub, file_name];
+
+                    fn_str = convertCharsToStrings(file_path);
+
+                    GenerateAtmosphere.result_files{a, e} = file_path;
+
+                    if all(2 == arrayfun(@(F) exist(F, 'file'), fn_str + extensions))
+                        continue
+                    end
+
+                    ialbdx = far_field_albedo( ...
                                 spectral_reflectance=spectral_reflectance, ...
                                 tilt=azimuth(a), ...
                                 wazim=elevation(e), ...
-                                ialbdg=foreground_local_albedo);
+                                ialbdg=foreground_local_albedo );
 
-                    file_name = ['az_', num2str(azimuth(a)), '_el_', num2str(elevation(e))];
-                    GenerateAtmosphere.result_files{a, e} = file_name;
 
                     SMARTS_conf = SMARTS_conf.update_card(ialbdx);
 
@@ -51,8 +86,30 @@ classdef GenerateAtmosphere
                                     file_name=file_name);
                 end
             end
+        end
 
 
+        function GenerateAtmosphere = ConstructAtmosphere(GenerateAtmosphere)
+            GenerateAtmosphere.atmosphere = cell(...
+                                        numel(GenerateAtmosphere.azimuth), ...
+                                        numel(GenerateAtmosphere.elevation));
+
+            for a = 1 : numel(GenerateAtmosphere.azimuth)
+                for e = 1 : numel(GenerateAtmosphere.elevation)
+                    file_path = [GenerateAtmosphere.result_files{a, e}, '.ext.txt'];
+                    data = readtable(file_path, 'VariableNamingRule', 'preserve');
+                    temp.azimuth = a;
+                    temp.elevation = e;
+                    temp.transmittance = data.Mixed_gas__transmittance;
+                    temp.global_tilted_irradiance = data.Global_tilted_irradiance;
+                    GenerateAtmosphere.atmosphere{a, e} = temp;
+                end
+            end
+        end
+
+        function ExportAtmosphere(GenerateAtmosphere, result_path)
+            atmosphere = GenerateAtmosphere.atmosphere;
+            save(result_path, 'atmosphere');
         end
 
     end
