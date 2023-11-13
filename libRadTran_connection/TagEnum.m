e7866f13b7a77f89a0ed56289cbbe29bdb3f2183
Peter Thomas Barrow <peter.barrow.93@gmail.com>
2023-11-13 14:44:00 +0000

    clean up

--- libRadTran_connection/TagEnum.m
+++
@@ -1,7 +0,0 @@
-classdef TagEnum
-    enumeration
-        IsFile,         IsFileName, IsValue,     IsOnOff, ...
-        IsOptionResult, IsPosition, IsCondition, IsObsolete, ...
-        IsArray,        IsSetScale
-    end
-end
