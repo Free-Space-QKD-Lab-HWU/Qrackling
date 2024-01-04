016a35c4e3427fd454221afffcfabb753cd0ad01
Cameron Simmons <ccs2000@hw.ac.uk>
2024-01-04 15:43:06 +0000

    Export PAA data to csv file

--- +beacon/@BeaconResult/BeaconResult.m
+++ +beacon/@BeaconResult/BeaconResult.m
@@ -94,7 +94,8 @@ classdef BeaconResult
             fig = figure("Name", ['Beacon simulation from ', ...
                 result.transmitter_name, ' to ', result.receiver_name], ...
                 "WindowState", "maximized");
-
+            
+            %% plot received power and SNR
             subplot(2, 3, [1,2])
             yyaxis left
             plot(x_axis(mask), result.received_power(mask));
@@ -105,12 +106,14 @@ classdef BeaconResult
             plot(x_axis(mask), result.snr_db(mask))
             ylabel("SNR (dB)");
             xlabel(x_label);
-
+            
+            %% plot link losses
             subplot(2, 3, [4,5])
             hold on
             result.losses.plotLosses(x_axis, x_label, "mask", mask);
             hold off
             
+            %% plot link heading and elevation
             polarax = subplot(2,3,3,polaraxes);
             polarplot(polarax,deg2rad(result.heading(mask)), result.elevation(mask))
             set(polarax,'ThetaZeroLocation','top',...
@@ -121,6 +124,7 @@ classdef BeaconResult
                                 'RTick',[0,30,60,90],...
                                 'RTickLabel',{'0^\circ','30^\circ','60^\circ','90^\circ'});
 
+            %% plot point ahead angles
             subplot(2,3,6)
             plot(result.PAA(1,mask), result.PAA(2,mask))
             %centre plot on zero with equal axes
@@ -137,6 +141,47 @@ classdef BeaconResult
 
         end
 
+        function ExportPAA(result,options)
+            % write point ahead angle as a function of time to a CSV file
+            % 
+            % entries are (comma and space ', ' separated):
+            % time (UTC in yyyy-MM-dd HH:mm:ss.SSSS format)
+            % heading PAA, in rads, fixed-point to 16 dp
+            % elevation PAA, in rads, fixed-point to 16 dp
+
+            % if the intended file already exists, ExportPAA appends the
+            % new information to the bottom
+            arguments
+                result beacon.BeaconResult
+                options.Name {mustBeText} = 'PAA.csv'
+            end
+
+            %format file name
+            if isstring(options.Name)
+                Name = char(options.Name);
+            else
+                Name = options.Name;
+            end
+            
+            if numel(Name)<4 || ~all(isequal(Name(end-3:end),'.csv'))
+                Name = [Name,'.csv'];
+            end
+
+           
+            %convert data to strings
+            timestring = string(result.time','yyyy-MM-dd HH:mm:ss.SSS');
+                %here we need to check whether times have been discretised
+                %so that many points have same time to second precision
+                milliseconds_from_start = round(seconds(result.time-result.time(1)),4);
+                assert(all(milliseconds_from_start(2:end)-milliseconds_from_start(1:end-1)>0),...
+                        'Time stamps must be uniformly increasing when recorded to second precision');
+
+            xstring = compose("%.16f",result.PAA(1,:)');
+            ystring = compose("%.16f",result.PAA(2,:)');
+
+            %write strings to file
+            writelines(strcat(timestring,', ',xstring,', ',ystring),Name,'WriteMode','append')
+        end
     end
 end
 
