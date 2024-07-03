10af31a168ca66f9c1e37d72a327c1e8e3f2caa0
Peter Thomas Barrow <peter.barrow.93@gmail.com>
2024-07-03 14:16:24 +0100

    refactor: load multiple presets
    multiple presets can now be used to create an array of detectors

--- +components/@DetectorPreset/DetectorPreset.m
+++ +components/@DetectorPreset/DetectorPreset.m
@@ -19,4 +19,41 @@ classdef DetectorPreset
             mustBeLessThanOrEqual(Efficiencies, 1)}
     end
 
+    methods
+
+        function detector = into_detector(preset, options)
+
+            arguments
+                preset components.DetectorPreset
+                options.n {mustBeGreaterThanOrEqual(options.n, 1)} = 1
+            end
+
+            if isscalar(preset)
+                wavelength = preset.Wavelength_Range(1);
+                clock_rate = 1e6;
+                time_gate_width = 1e-9;
+                filter_spectral = components.IdealBPFilter(wavelength, 1);
+
+                detector = components.Detector(wavelength, clock_rate, time_gate_width, ...
+                                              filter_spectral, "Preset", preset );
+                return
+            end
+
+            n = numel(preset);
+            detector =  components.Detector.empty(0, n);
+
+            for i = 1:n
+                wavelength = preset(i).Wavelength_Range(i);
+                clock_rate = 1e6;
+                time_gate_width = 1e-9;
+                filter_spectral = components.IdealBPFilter(wavelength, 1);
+
+                detector(i) = components.Detector(wavelength, clock_rate, time_gate_width, ...
+                                              filter_spectral, "Preset", preset(i) );
+            end
+
+        end
+
+    end
+
 end
